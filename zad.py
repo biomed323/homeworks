ed19ca9381b5146f6070514b2bea9a04810bb2b5
biomed323 <48203468+biomed323@users.noreply.github.com>
2023-11-30 11:03:16 +0100

    Add files via upload

---
+++ zad.py
@@ -0,0 +1,35 @@
+# my_str = "abcdefghij"
+# print("a.", my_str[2])
+# print("b.", my_str[-2])
+# print("c.", my_str[:5])
+# print("d.", my_str[:-2])
+# print("e.", my_str[::2])
+# print("f.", my_str[1::2])
+# print("g.", my_str[::-1])
+# print("h.", my_str[::-2])
+# print("i.", len(my_str))
+
+
+# word = "Words of legends"
+# print(word.count(" ")+1)
+
+
+# s = input("Введіть рядок: ")
+# ch = input("Введіть символ: ")
+#
+# index = s.find(ch)
+# while index != -1:
+#     print(f"Символ '{ch}' знайдено на позиції {index}")
+#     index = s.find(ch, index + 1)
+
+
+s = "halohalo"
+s = s.replace("h", "H")
+print(s)
+
+
+
+
+
+
+
