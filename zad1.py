1645e2316810a9342b8fb1f09fc47c60675a1567
biomed323 <48203468+biomed323@users.noreply.github.com>
2023-11-30 10:49:36 +0100

    Add files via upload

---
+++ zad1.py
@@ -0,0 +1,26 @@
+#original_value = 120
+#new_value = original_value / 2 if original_value < 100 else -original_value
+#print(new_value)
+
+
+# original_value = 120
+# new_value = 1 if original_value < 100 else 0
+# print(new_value)
+
+
+# original_value = 120
+# new_value = True if original_value < 100 else False
+# print(new_value)
+
+
+# my_str = "qwer"
+# my_str = my_str + my_str if len(my_str) < 5 else my_str
+# print(my_str)
+
+
+my_str = "qwer"
+my_str = my_str + my_str[::-1] if len(my_str) < 5 else my_str
+print(my_str)
+
+
+
